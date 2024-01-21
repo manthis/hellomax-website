953159f46ed832a7c0415f4cd01b45329f03a0a1
Maxime AUBURTIN <maxime@auburt.in>
2024-01-21 19:56:17 +0100

    Small fixes on sizes

--- app/components/homecontent/content.tsx
+++ app/components/homecontent/content.tsx
@@ -4,9 +4,9 @@ export default function Content() {
     return (
         <div id='content' className='relative z-10 flex w-full origin-left items-center justify-between duration-500'>
             <div className='relative z-50 max-w-[600px]'>
-                <h4 className='mb-2 text-2xl font-normal tracking-widest text-lightblue landscape:text-lg '>Hello,</h4>
-                <h2 className='text-7xl leading-[1em] text-white landscape:text-5xl'>I&apos;m Max</h2>
-                <h3 className='text-3xl font-light leading-[1.5em] text-white landscape:text-xl'>
+                <h4 className='text-2xl font-normal tracking-widest text-lightblue landscape:text-lg '>Hello,</h4>
+                <h2 className='my-4 text-7xl leading-[1em] text-white landscape:text-5xl'>I&apos;m Max</h2>
+                <h3 className='text-5xl font-light leading-[1.5em] text-white landscape:text-xl'>
                     Blockchain CTO & Full Stack Engineer
                 </h3>
                 <p className='mb-[20px] mt-[10px] text-base text-white landscape:text-sm'>
